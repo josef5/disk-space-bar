83efe2ab8dcd2e120f2f1cfc465d72ce6c63adbc
Jose Espejo <joseespejo@yahoo.com>
2026-02-18 17:33:52 +0000

    Initial Commit

---
+++ DiskSpaceBar/DiskSpaceBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskSpaceBarApp.swift
+//  DiskSpaceBar
+//
+//  Created by Jose Espejo on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct DiskSpaceBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
